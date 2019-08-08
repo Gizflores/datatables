32c59caefcdf70fce96333a2bde97aaf44937b61
Diego Flores <diegooflores14@gmail.com>
2019-08-08 15:05:59 -0600

    Datatable first try

---
+++ resources/views/index.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
+
+    <title>DataTables</title>
+</head>
+    <body>
+        <table id="users" class="table-striped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nombre</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach($users as $user)
+            <tr>
+              <td>{{$user->id}}</td>
+              <td>{{$user->name}}</td>
+              <td>{{$user->email}}</td>
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+
+      <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+      <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+      <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+      <script>
+      $(document).ready(function() {
+        $('#users').DataTable();
+        } );
+      </script>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+use App\User;
+use App\UsersController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+
+  $users = User::all();
+  //dd($users);
+  return view('index',compact('users'));
+});
